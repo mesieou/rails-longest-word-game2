d99542715ef38b11f16ee74dd568cf7593bc5299
juan bernal <mesieou@gmail.com>
2024-01-15 20:33:42 +1100

    api call implemented

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,7 @@
-class GamesController < ApplicationController
+require "json"
+require "open-uri"
 
+class GamesController < ApplicationController
   def generate_letters
     letters = []
     quantity = (4..10).to_a.sample
@@ -17,17 +19,20 @@ class GamesController < ApplicationController
   end
 
   def score
+    submitted_word = params[:word]
     submitted_letters = params[:word].downcase.split('')
     computer_letters = params[:letters].downcase.split('')
 
-    @result = display_message(computer_letters, submitted_letters)
+    @result = display_message(computer_letters, submitted_letters, submitted_word)
   end
 
-  def display_message(computer_letters, submitted_letters)
-    if valid_word?(computer_letters, submitted_letters)
-      'Congratulations'
+  def display_message(computer_letters, submitted_letters, submitted_word)
+    if !valid_word?(computer_letters, submitted_letters)
+      'Sorry, some of the letters/letter are/is not in the grid given'
+    elsif !english_word?(submitted_word)
+      'Sorry, this is not a valid english word'
     else
-      'Sorry'
+      'Congratulations!'
     end
   end
 
@@ -36,5 +41,12 @@ class GamesController < ApplicationController
       computer_letters.include?(letter) &&
         submitted_letters.count(letter) <= computer_letters.count(letter)
     end
+  end
+
+  def english_word?(submitted_word)
+    url = "https://wagon-dictionary.herokuapp.com/#{submitted_word}"
+    word_serialised = URI.open(url).read
+    word_from_api = JSON.parse(word_serialised)
+    word_from_api['found']
   end
 end
